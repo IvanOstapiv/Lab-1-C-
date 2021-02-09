1e2f8cb516567a085b40de447d8c2bcff7ae160c
IvanOstapiv <65124766+IvanOstapiv@users.noreply.github.com>
2021-02-09 21:08:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace My_project
+{
+    public class Program
+    {
+        static public int Lab(int a, int b = 60)
+        {
+            int c = a / b;
+            Console.WriteLine(a.ToString() + " / " + b + " = " + c.ToString());
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть кількість секунд");
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+            Lab(a);
+            Console.ReadLine();
+        }
+    }
+}
